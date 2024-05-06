eda00d520cd8b9bc5e379ec99fbee25da9b59e71
Kelilopz <157431779+Kelilopz@users.noreply.github.com>
2024-05-06 21:18:38 +0000

    feat: :construction: Cambios en la maquetacion
    Se realizan cambios en la maquetacion incluyendo las tablas

--- js/modulesComponents/title.js
+++ js/modulesComponents/title.js
@@ -2,4 +2,4 @@ export const nameRockets = async(name)=>{
     let header__title = document.querySelector("#header__title");
     header__title.innerHTML = "";
     header__title.textContent = name;
-}
+}createElement
